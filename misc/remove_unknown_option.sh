c3fd00f04e6e6984b5d904b1c326bc394d52d78a
Han Sangjin <tinysun.net@gmail.com>
2016-03-05 19:01:05 +0900

    Patch MSVC project file post processor

--- misc/remove_unknown_option.sh
+++ misc/remove_unknown_option.sh
@@ -6,8 +6,26 @@ do
   if [ $? -eq 0 ]
   then
     echo "Exist in $f"
-    sed -e 's; -w14062 /Zc:inline /EHs-c- ;;' -e 's; -target x86_64-pc-windows-msvc -isysroot -L; -L;' $f > tmp.out
+    sed -e 's; -w14062 /Zc:inline /EHs-c- ;;' -e 's; -target x86_64-pc-windows-msvc -isysroot -L; -L;' $f  > tmp.out
     mv tmp.out $f
     echo $f "changed"
+  fi
+  grep '_HAS_EXCEPTIONS=0' $f > /dev/null
+  if [ $? -eq 0 ]
+  then
+    sed -e 's/_HAS_EXCEPTIONS=0;//g' $f  > tmp.out
+    mv tmp.out $f
+  fi
+  grep 'WIN32;' $f > /dev/null
+  if [ $? -eq 0 ]
+  then
+    sed -e 's/WIN32;/__STDC__=0;WIN32;/' $f  > tmp.out
+    mv tmp.out $f
+  fi
+  grep 'target x86_64' $f > /dev/null
+  if [ $? -eq 0 ]
+  then
+    sed -e 's/-target/-Wno-macro-redefined -target/' $f  > tmp.out
+    mv tmp.out $f
   fi
 done
