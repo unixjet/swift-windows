0463c9bf7265dc7be4e49324911065c9689040cb
Han Sangjin <tinysun.net@gmail.com>
2016-04-30 20:35:35 +0900

    Fix dllimport injector

--- misc/inject_dllimport.py
+++ misc/inject_dllimport.py
@@ -2,20 +2,20 @@
 import sys
 import re
 
-replace_map = { \
-  r'external global':'external dllimport global', \
-  r'external constant':'external dllimport constant', \
-  r'@_TWVBo = external dllimport':'@_TWVBo = external', \
-  r'@_TWVBo':'@__imp__TWVBo' \
-  }
+replace_map = [ \
+  (r'external global', 'external dllimport global'), \
+  (r'external constant', 'external dllimport constant'), \
+  (r'@_TWVBo = external dllimport', '@_TWVBo = external'), \
+  (r'@_TWVBo', '@__imp__TWVBo') \
+  ]
 
 newf = open(sys.argv[2], "w")
 with open(sys.argv[1], "r") as sources:
   lines = sources.readlines()
   for line in lines:
     newline = line
-    for k in replace_map.keys():
-      newline = re.sub(k, replace_map[k], newline)
+    for (exp, newstr) in replace_map:
+      newline = re.sub(exp, newstr, newline)
 #    print newline,
     newf.write(newline)
 newf.close()
