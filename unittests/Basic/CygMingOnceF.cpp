5687dabb70c84dcd92ffa1666d50e859cd3405dc
Han Sangjin <tinysun.net@gmail.com>
2016-08-03 08:16:36 +0900

    Cygwin test script starts to work.
    cmake --build <swift-build-dir> -- check-swift

---
+++ unittests/Basic/CygMingOnceF.cpp
@@ -0,0 +1,55 @@
+//===--- CygwinPort.cpp - Functions for Cygwin port -----------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// Implementations Cygwin specific functions needed for running Swift.
+//
+//===----------------------------------------------------------------------===//
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+#include "swift/Runtime/Debug.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mutex>
+#include <vector>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <psapi.h>
+
+namespace swift {
+void _swift_once_f(uintptr_t *predicate, void *context,
+                          void (*function)(void *));
+}
+
+using namespace swift;
+
+#if !defined(_MSC_VER)
+static std::mutex swiftOnceMutex;
+
+void swift::_swift_once_f(uintptr_t *predicate, void *context,
+                          void (*function)(void *)) {
+  // FIXME: This implementation does a global lock, which is much worse than
+  // what we have on other platforms. Each swift_once should synchronize on the
+  // token.
+  swiftOnceMutex.lock();
+  if (*predicate == 0) {
+    *predicate = 1ul;
+    swiftOnceMutex.unlock();
+
+    function(context);
+  } else
+    swiftOnceMutex.unlock();
+}
+#endif
+#endif
